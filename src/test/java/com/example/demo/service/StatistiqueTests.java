f3fdebcbac556697f68ea88f95766b6d05a890ec
SanjaiV2 <sanjai.gfr@gmail.com>
2026-06-17 10:11:03 +0200

    Service améliorer

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -10,25 +10,68 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest
-public class StatistiqueTests {
+package com.example.demo.service;
 
-    @MockBean
-    StatistiqueImpl statistiqueImpl;
+import com.example.demo.data.Voiture;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class StatistiqueTests {
 
     @Test
-    void ajouterDeuxVoitures() throws ArithmeticException {
-        Voiture v1 = new Voiture("BMW", 20000);
-        Voiture v2 = new Voiture("Audi", 30000);
+    void testMockito() {
 
-        statistiqueImpl.ajouter(v1);
-        statistiqueImpl.ajouter(v2);
+        Statistique statistique = mock(Statistique.class);
 
-        when(statistiqueImpl.prixMoyen()).thenReturn(new Echantillon(2, 25000));
+        when(statistique.prixMoyen())
+                .thenReturn(new Echantillon(2,25000));
 
-        Echantillon e = statistiqueImpl.prixMoyen();
+        Echantillon e = statistique.prixMoyen();
+
+        assertEquals(2, e.getNombreDeVoitures());
+        assertEquals(25000, e.getPrixMoyen());
+
+        verify(statistique).prixMoyen();
+    }
+
+    @Test
+    void testPrixMoyen() {
+
+        StatistiqueImpl s = new StatistiqueImpl();
+
+        s.ajouter(new Voiture("BMW",20000));
+        s.ajouter(new Voiture("Audi",30000));
+
+        Echantillon e = s.prixMoyen();
 
         assertEquals(2, e.getNombreDeVoitures());
         assertEquals(25000, e.getPrixMoyen());
     }
+
+    @Test
+    void testPrixMoyenException() {
+
+        StatistiqueImpl s = new StatistiqueImpl();
+
+        assertThrows(ArithmeticException.class,
+                () -> s.prixMoyen());
+    }
+
+    @Test
+    void testEchantillonVide() {
+
+        Echantillon e = new Echantillon();
+
+        e.setNombreDeVoitures(3);
+        e.setPrixMoyen(15000);
+
+        assertEquals(3, e.getNombreDeVoitures());
+        assertEquals(15000, e.getPrixMoyen());
+    }
+}
+
+
 
 }
